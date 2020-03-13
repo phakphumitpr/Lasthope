1bccaa1aa90c3b9dc8b92b93342bb85b65d43996
phakphumitpr <47375501+phakphumitpr@users.noreply.github.com>
2020-03-13 16:38:43 +0700

    Initial commit

---
+++ application/config/autoload.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+
+
+$autoload['libraries'] = array('database','session','form_validation','upload');
+
+
+$autoload['drivers'] = array();
+
+
+$autoload['helper']    = array('url','html','file','form');
+
+
+$autoload['config'] = array();
+
+
+$autoload['language'] = array();
+
+
+$autoload['model'] = array();
